b5f1a1dbae271a0a6d3eb1af8f5cf99213df677a
Kerriea-star <ojared511@gmail.com>
2022-03-28 11:46:49 +0300

    Created our customManager model

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,5 +1,30 @@
 from email.policy import default
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from numpy import right_shift
+
+
+class customManager(BaseUserManager):
+    def create_user(self, email, username, password=None, is_admin=False, is_active=True, is_staff=False, is_student=False, is_teacher=False):
+        if not email:
+            raise ValueError("Users must have an email!")
+        if not password:
+            raise ValueError("Users must have a password")
+        if not username:
+            raise ValueError("Users must have a username")
+
+        user_obj = self.model(
+            email = self.normalize_email(email), username=username
+        )
+        
+        user_obj.set_password(password)
+        user_obj.is_active = is_active
+        user_obj.is_admin = is_admin
+        user_obj.is_staff = is_staff
+        user_obj.is_teacher = is_teacher
+        user_obj.is_student = is_student
+        user_obj.save(using=self._db)
+        return user_obj
 
 class User(AbstractBaseUser):
     username = models.CharField(max_length=63, unique=True)
@@ -14,3 +39,8 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.username
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = customManager()
