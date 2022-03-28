94ceaad7adbfa67d30822b84b10cde48120b77ff
Kerriea-star <ojared511@gmail.com>
2022-03-28 11:24:46 +0300

    Started creating our custom models

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,3 +1,16 @@
+from email.policy import default
 from django.db import models
 
-# Create your models here.
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=63, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
